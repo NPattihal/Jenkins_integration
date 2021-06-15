dcde94e15c03c1b92a5856362b7c8ccb0e774d97
Nupura Pattihal <Nupura.Pattihal@asos.com>
2021-06-15 14:24:32 +0530

    initial

---
+++ Jenkins_integration/src/integratejenkins/sample.java
@@ -0,0 +1,12 @@
+package integratejenkins;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("Great news! You have integrated your project with jenkins!");
+			System.out.println("Great news! You have integrated your project with jenkins!");
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Jenkins_integration
+#practice project
